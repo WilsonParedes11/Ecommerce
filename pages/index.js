af48d8b303b5ebae95e0d70ac89806e7afdd4bc4
WILSON PAREDES <wilsonparedes87@gmail.com>
2022-09-29 19:07:16 -0500

    development of components products and hero Banner

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 import HeroBanner from '../components/HeroBanner';
+import Product from '../components/Product';
+import FooterBanner from '../components/FooterBanner';
+
+
 import { client } from '../lib/client';
 
-const Home = ({products, bannerData}) => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
 
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      
+
       <div className='products-heading'>
         <h2>Beset Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </div>
   )
 }
